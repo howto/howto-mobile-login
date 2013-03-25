1b168a176c9f58791d3538c9c9c836dbcd747c03
Renato <renato.ibanhez@gmail.com>
2013-03-24 23:41:53 -0300

    Implementação do login com facebook e google.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'dF6Om0jFqgSd2Atb7HX51g', 'XRYHTY4zgCcZGauFJgupcTtUw0P7rFTM3ru0tHA0lB0'
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
-  provider :google_oauth2, "AIzaSyB7prI2SjfoQ6TvZCR8hhBZC-M-_hebPY0", "oRkAC2gMGLaoVCVi7bkhWmGg", { access_type: "offline", approval_prompt: "" }
+  provider :facebook, '560943807273111', 'd3bab44bb5cd30389c3e1d0ef0cfd83b'
+  provider :google_oauth2, '143011769534.apps.googleusercontent.com', 'oRkAC2gMGLaoVCVi7bkhWmGg' #, { access_type: 'offline', approval_prompt: '' }
 end
